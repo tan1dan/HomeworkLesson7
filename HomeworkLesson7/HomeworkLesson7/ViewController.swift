2ae80d8c64ebdac3f92256e6ae4c7c04da28a0b2
Ivan Znak <ivanznak04@gmail.com>
2023-10-26 23:51:36 +0200

    Was added classes Snack, MainMenu, Drinks, Desserts

--- HomeworkLesson7/HomeworkLesson7/ViewController.swift
+++ HomeworkLesson7/HomeworkLesson7/ViewController.swift
@@ -7,11 +7,72 @@
 
 import UIKit
 
+class Menu {
+    var snack: Product
+    var mainMenu: Product
+    var drink: Product
+    var dessert: Product
+    init(snack: Product, mainMenu: Product, drink: Product, dessert: Product) {
+        self.snack = snack
+        self.mainMenu = mainMenu
+        self.drink = drink
+        self.dessert = dessert
+    }
+}
+
+class Product{
+    
+}
+
+class Snack: Product{
+    var price: Int
+    var name: String
+    init(price: Int, name: String) {
+        self.price = price
+        self.name = name
+    }
+}
+
+class MainMenu: Product{
+    var price: Int
+    var name: String
+    init(price: Int, name: String) {
+        self.price = price
+        self.name = name
+    }
+}
+
+class Drinks: Product{
+    var price: Int
+    var name: String
+    init(price: Int, name: String) {
+        self.price = price
+        self.name = name
+    }
+}
+
+class Deserts: Product{
+    var price: Int
+    var name: String
+    init(price: Int, name: String) {
+        self.price = price
+        self.name = name
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let snack = Product(price: 10, name: "Snikers")
+        let mainMenu = Product(price: 30, name: "Beef")
+        let drink = Product(price: 20, name: "Latte")
+        let dessert = Product(price: 20, name: "Cake")
+        let menu = Menu(snack: snack, mainMenu: mainMenu, drink: drink, dessert: dessert)
+        func printOutMeal(_ product: Product){
+            print("Name: '\(product.name)' and price: '\(product.price)'")
+        }
+        printOutMeal(mainMenu)
     }
 
 
